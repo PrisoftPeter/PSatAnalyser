fcfca3e5f34fc6ba19d3fb4fc777a5790641bf24
inah <omoronyia@gmail.com>
2017-07-16 00:04:04 +0100

    psatconfig

--- src/main/java/gla/prisoft/client/kernel/display/model/PSatConfigView.java
+++ src/main/java/gla/prisoft/client/kernel/display/model/PSatConfigView.java
@@ -435,7 +435,9 @@ class AnalysisConfig extends javax.swing.JPanel {
         decisionCatLimit_cb1 = new javax.swing.JComboBox<>();
 
         jLabel1.setText("Max. # of objects on a path:");
-	    Integer maxPathLengths[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	    Integer maxPathLengths[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,
+	    							22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
+	    							40,41,42,43,44,45,46,47,48,49,50};
 	    maxPathLength_cb = new javax.swing.JComboBox<Integer>(maxPathLengths);
         for(int i=0;i<maxPathLengths.length;i++){
 	    	if(maxPathLengths[i] == PSatAPI.instance.max_analysis_path_length){
